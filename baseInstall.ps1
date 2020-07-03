248469a96ea61bc569e2f1f77b83b52ac194986d
Mikkel Kaas <mikkel.kaas.64@gmail.com>
2020-07-03 08:51:43 +0200

    Initial commit

---
+++ baseInstall.ps1
@@ -0,0 +1,10 @@
+echo "Installing System tools"
+iex "choco install 7zip -y"
+
+echo "Installing Editors"
+iex "choco install notepadplusplus -y"
+iex "choco install vscode -y"
+
+echo "Installing Script languages"
+iex "choco install python3 -y"
+iex "choco install jre8 -y"
